dbf124f17d72f3a65537c24c3c59aa5f3ed8cfff
meloncoder <melonCoder@126.com>
2016-09-24 19:42:31 +0800

    fix redundant operation

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/melonCoder/MCvim ~/.vim/MCvim
-ln .vim/MCvim/vimrc .vimrc
-ln .vim/MCvim/vimrc.local .vimrc.local
+ln vimrc ~/.vimrc
+ln vimrc.local ~/.vimrc.local
