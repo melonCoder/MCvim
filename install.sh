0507df0efade3998b3f98aa701d988de9b20a8a0
meloncoder <melonCoder@126.com>
2016-09-24 19:31:39 +0800

    enable auto installation in linux

---
+++ install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/melonCoder/MCvim ~/.vim/MCvim
+ln .vim/MCvim/vimrc .vimrc
+ln .vim/MCvim/vimrc.local .vimrc.local
